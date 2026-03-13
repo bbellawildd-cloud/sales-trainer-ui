9ed6391f96b3c0a6b7d703d703851181b9e90197
bbellawildd-cloud <bbellawildd@gmail.com>
2026-03-13 16:03:10 -0400

    Update ProspectAvatar.js

--- components/ProspectAvatar.js
+++ components/ProspectAvatar.js
@@ -1,63 +1,45 @@
 import { useEffect, useState } from "react";
 
 export default function ProspectAvatar({ speaking }) {
-
-const talkFrames = ["talk1","talk2","talk3","talk2"];
+const talkFrames = ["talk1", "talk2", "talk3", "talk2"];
 const [frame, setFrame] = useState("idle");
 
-
-// blink animation
 useEffect(() => {
-
 const blink = setInterval(() => {
-
 setFrame("blink");
 
 setTimeout(() => {
 setFrame("idle");
-},150);
-
-},4000);
+}, 150);
+}, 4000);
 
 return () => clearInterval(blink);
+}, []);
 
-},[]);
-
-
-// talking animation
 useEffect(() => {
-
-if(!speaking) return;
+if (!speaking) {
+setFrame("idle");
+return;
+}
 
 let i = 0;
 
 const talk = setInterval(() => {
-
 setFrame(talkFrames[i % talkFrames.length]);
-
-i++;
-
-},120);
+i += 1;
+}, 120);
 
 return () => clearInterval(talk);
-
-},[speaking]);
-
+}, [speaking]);
 
 return (
-
-<div style={{width:140,height:140}}>
-
+<div className="avatarContainer">
 <img
 src={`/prospect/${frame}.png`}
-style={{
-width:120,
-borderRadius:16
-}}
+className={`avatar ${speaking ? "talking" : ""}`}
+alt="prospect"
 />
-
 </div>
-
 );
-
 }
+
