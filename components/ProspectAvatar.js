052b4bee90f8c07437a9183f31042ea53bd033be
bbellawildd-cloud <bbellawildd@gmail.com>
2026-03-17 17:27:22 -0400

    Update ProspectAvatar.js

--- components/ProspectAvatar.js
+++ components/ProspectAvatar.js
@@ -1,8 +1,22 @@
-export default function ProspectAvatar({ speaking }) {
-  return (
-    <div className={'avatarAlive ${speaking ? "avatarSpeaking" : ""}'}>
-      <img src="/idle.png" width={120} alt="avatar" className="avatarImg" />
-   </div>
-  );
-}
+import { useEffect, useState } from "react";
 
+export default function ProspectAvatar({ speaking, emotion = "idle" }) {
+const [tilt, setTilt] = useState(0);
+
+useEffect(() => {
+const interval = setInterval(() => {
+setTilt((Math.random() - 0.5) * 6); // subtle head movement
+}, 2000);
+
+return () => clearInterval(interval);
+}, []);
+
+return (
+<div
+className={`avatarAlive ${speaking ? "avatarSpeaking" : ""}`}
+style={{ transform: `rotate(${tilt}deg)` }}
+>
+<img src="/idle.png" width={120} className="avatarImg" />
+</div>
+);
+}
